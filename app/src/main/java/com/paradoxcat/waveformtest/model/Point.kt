24a94ac8bdaf47ef691b082d340393d53d079332
Louise Malvin Tanaka <louise.malvin12@gmail.com>
2023-07-30 19:57:10 +0200

    feat: Major rework on waveform render
    - Reverted drawing algorithm back from point x1,y1 to x2,y2
    - Added early implementation to render waveform animation

---
+++ app/src/main/java/com/paradoxcat/waveformtest/model/Point.kt
@@ -0,0 +1,6 @@
+package com.paradoxcat.waveformtest.model
+
+data class Point(
+    val x: Float,
+    val y: Float
+)
--- app/src/main/java/com/paradoxcat/waveformtest/waveviewer/WaveformSlideBar.kt
+++ app/src/main/java/com/paradoxcat/waveformtest/waveviewer/WaveformSlideBar.kt
@@ -1,11 +1,15 @@
 package com.paradoxcat.waveformtest.waveviewer
-
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import com.paradoxcat.waveformtest.model.Point
+import kotlin.math.floor
 import kotlin.math.pow
 
 /**
@@ -20,69 +24,94 @@ class WaveformSlideBar(context: Context, attrs: AttributeSet) : View(context, at
         const val LEFT_RIGHT_PADDING = 50.0f
         const val TOP_BOTTOM_PADDING = 50.0f
         const val MIRROR_SAMPLES = true
-        const val LINE_WIDTH = 1.0f
-        const val DEFAULT_STEP_COUNT = 1000
+        const val LINE_WIDTH = 2.0f
+        const val DEFAULT_STEP_COUNT = 2000
         private val MAX_VALUE = 2.0f.pow(16.0f) - 1 // max 16-bit value
         val INV_MAX_VALUE = 2.0f / MAX_VALUE // multiply with this to get % of max value
 
-        /**
-         * Calculate points to draw for drawLines() method.
-         * @param mirrored -- if true, line will be drawn from y to -y, otherwise from y to middle
-         * @param waveform -- audio data in IntArray format
-         * @param width -- width of the view
-         * @param height -- height of the view
-         * @param stepCount -- how many samples to skip to draw maxLines
-         * @return -- floatArray of x0, y0, x1, y1, ...
-         */
-        fun calculateDrawLinesPoints(
-            waveform: IntArray,
-            mirrored: Boolean,
-            width: Int,
-            height: Int,
-            stepCount: Int
-        ): FloatArray {
-            var result = floatArrayOf()
-            val sampleDistance =
-                (width - LEFT_RIGHT_PADDING * 2) / (waveform.size - 1) // distance between centers of 2 samples
-            val canvasWidth = width - LEFT_RIGHT_PADDING * 2 // width of the canvas minus paddings
-            val maxAmplitude =
-                height / 2.0f - TOP_BOTTOM_PADDING // max amount of px from middle to the edge minus pad
-            val amplitudeScaleFactor =
-                INV_MAX_VALUE * maxAmplitude // multiply by this to get number of px from middle
-
-            // calculate points for drawLines()
-            for (i in 0 until waveform.size step stepCount) {
-                // TODO: fine-tune y-values with skipped samples
-                val y = (height / 2.0f - waveform[i] * amplitudeScaleFactor)
-                val x = LEFT_RIGHT_PADDING + i * sampleDistance
-                // draw a straight line from y to middle or -y depending on mirrored flag
-                result = if (mirrored) {
-                    result.plus(x).plus(height - y).plus(x).plus(y)
-                } else {
-                    result.plus(x).plus(height / 2.0f).plus(x).plus(y)
-                }
-            }
-
-            return result
-        }
-
     }
 
+    private lateinit var waveform: Path
+
     private val linePaint = Paint()
-    private lateinit var waveForm: FloatArray
+    private val animator: ValueAnimator
+    private var indexOfDrawnPoints: Int = 0
 
     init {
         linePaint.color = Color.rgb(0, 0, 0)
         linePaint.strokeWidth = LINE_WIDTH
+        linePaint.style = Paint.Style.FILL_AND_STROKE
         linePaint.strokeCap = Paint.Cap.ROUND
+        animator = ValueAnimator.ofFloat(0.0f, 1.0f)
+        animator.interpolator = AccelerateDecelerateInterpolator()
+        animator.duration = 2000
+    }
+    /**
+     * Calculate points to draw for drawLines() method.
+     * @param waveform -- audio data in IntArray format
+     * @param width -- width of the view
+     * @param height -- height of the view
+     * @param stepCount -- how many samples to skip to draw maxLines
+     */
+    private fun calculatePathPoints(
+        waveform: IntArray,
+        width: Int,
+        height: Int,
+        stepCount: Int
+    ): Array<Point> {
+        var result = arrayOf<Point>()
+        val sampleDistance =
+            (width - LEFT_RIGHT_PADDING * 2) / (waveform.size - 1) // distance between centers of 2 samples
+        val canvasWidth = width - LEFT_RIGHT_PADDING * 2 // width of the canvas minus paddings
+        val maxAmplitude =
+            height / 2.0f - TOP_BOTTOM_PADDING // max amount of px from middle to the edge minus pad
+        val amplitudeScaleFactor =
+            INV_MAX_VALUE * maxAmplitude // multiply by this to get number of px from middle
+
+        // calculate points for drawLines()
+        for (i in waveform.indices step stepCount) {
+            // TODO: fine-tune y-values with skipped samples
+            val x = LEFT_RIGHT_PADDING + i * sampleDistance
+            val y = (height / 2.0f - waveform[i] * amplitudeScaleFactor)
+            val point = Point(x, y)
+            // draw a straight line from y to middle or -y depending on mirrored flag
+            result = result.plus(point)
+        }
+        return result
+    }
+
+    private fun preparePath(points: Array<Point>): Path {
+        val result = Path()
+        var prevX = points.first().x
+        var prevY = points.first().y
+        result.moveTo(prevX, prevY)
+        for (point in points) {
+            val x = point.x
+            val y = point.y
+            val controlX1 = (prevX + x) / 2
+            val controlY1 = (prevY + y) / 2
+            result.quadTo(controlX1, controlY1, x, y)
+            prevX = x
+            prevY = y
+        }
+        return result
+    }
+
+    private fun render(points: Array<Point>, animationValue: Float) {
+        val nextDrawIndex : Int = floor(points.size * animationValue).toInt() - 1
+        if (nextDrawIndex <= indexOfDrawnPoints) return
+        val pointsToDraw = points.copyOfRange(indexOfDrawnPoints, nextDrawIndex)
+        indexOfDrawnPoints = nextDrawIndex + 1
+        waveform.addPath(preparePath(pointsToDraw))
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        if (::waveForm.isInitialized) {
-            canvas?.drawLines(waveForm, linePaint)
-            canvas?.save()
+        canvas?.save()
+        if (::waveform.isInitialized) {
+            canvas?.drawPath(waveform, linePaint)
         }
+        canvas?.restore()
     }
 
     /**
@@ -91,7 +120,16 @@ class WaveformSlideBar(context: Context, attrs: AttributeSet) : View(context, at
      *                  not matter, since we are not rendering any time-related information yet.
      */
     fun setData(intArray: IntArray) {
-        waveForm = calculateDrawLinesPoints(intArray, MIRROR_SAMPLES, width, height, DEFAULT_STEP_COUNT)
-        invalidate()
+        val points = calculatePathPoints(intArray, width, height, DEFAULT_STEP_COUNT)
+        waveform = Path()
+        indexOfDrawnPoints = 0
+        animator.apply {
+            addUpdateListener { animation ->
+                val phase = animation.animatedValue as Float
+                render(points, phase)
+                invalidate()
+            }
+        }
+        animator.start()
     }
 }
